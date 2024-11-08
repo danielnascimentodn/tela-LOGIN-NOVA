15b0743cec200d31f69f3d3dc1e61f6d956c2499
danielnascimentodn <danielnascimento066@gmail.com>
2024-11-07 21:51:10 -0300

    NOVA TELA

---
+++ SCRIPTS/cadastro.js
@@ -0,0 +1,29 @@
+let btn = document.querySelector('#verSenha')
+let btnConfim = document.querySelector('#verConfirmSenha')
+
+let nome = document.querySelector('#nome')
+let labelNome = document.querySelector('#labelNome')
+let validNome = false;
+
+let usuario = document.querySelector('#usuario')
+let labelUsuario = document.querySelector('#labelUsuario')
+let validUsuario = false
+
+let senha =document.querySelector('#senha')
+let labelSenha = document.querySelector('#labelSenha')
+let validSenha = false
+
+let confirmSenha = document.querySelector('#confirmSenha')
+let labelConfirmSenha = document.querySelector('#labelConfirmSenha')
+let validConfirmSenha = false
+
+let msgError = document.querySelector('#msgError')
+let msgSucess = document.querySelector('#msgSucess')
+
+// -------------------------------------------------//
+
+nome.addEventListener('keyup', () => {
+    if(nome. value. length <= 2 )
+
+}
+)
