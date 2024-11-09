36ceccf7a681300329462c772e9f2d32da75ef50
danielnascimentodn <danielnascimento066@gmail.com>
2024-11-08 21:39:01 -0300

    cadastro de usuario

--- SCRIPTS/cadastro.js
+++ SCRIPTS/cadastro.js
@@ -1,5 +1,5 @@
-let btn = document.querySelector('#verSenha')
-let btnConfim = document.querySelector('#verConfirmSenha')
+let btn = document.querySelector('#verpassword')
+let btnConfim = document.querySelector('#verconfirmpassword')
 
 let nome = document.querySelector('#nome')
 let labelNome = document.querySelector('#labelNome')
@@ -9,12 +9,12 @@ let usuario = document.querySelector('#usuario')
 let labelUsuario = document.querySelector('#labelUsuario')
 let validUsuario = false
 
-let senha =document.querySelector('#senha')
-let labelSenha = document.querySelector('#labelSenha')
+let senha =document.querySelector('#password')
+let labelSenha = document.querySelector('#labelpassword')
 let validSenha = false
 
-let confirmSenha = document.querySelector('#confirmSenha')
-let labelConfirmSenha = document.querySelector('#labelConfirmSenha')
+let confirmSenha = document.querySelector('#confirm-password')
+let labelConfirmSenha = document.querySelector('#labelConfirm-password')
 let validConfirmSenha = false
 
 let msgError = document.querySelector('#msgError')
@@ -23,7 +23,86 @@ let msgSucess = document.querySelector('#msgSucess')
 // -------------------------------------------------//
 
 nome.addEventListener('keyup', () => {
-    if(nome. value. length <= 2 )
-        labelUsuario.setAttribute('style', 'color: green')
+    if(nome. value. length <= 2 ) {
+        labelNome.setAttribute('style', 'color: red')
+        labelNome.innerHTML = 'Nome *Insira um nome válido'
+        nome.setAttribute('style', 'border-color:red')
+        validNome = false
+    } else {
+        labelNome.setAttribute('style', 'color: green')
+        labelNome.innerHTML = 'Nome'
+        nome.setAttribute('style', 'border-color: green')
+        validNome = true
+    }   
+})
+
+usuario.addEventListener('keyup', () => {
+    if(usuario.value.length <= 4){
+      labelUsuario.setAttribute('style', 'color: red')
+      labelUsuario.innerHTML = 'Usuário *Insira no minimo 5 caracteres'
+      usuario.setAttribute('style', 'border-color: red')
+      validUsuario = false
+    } else {
+      labelUsuario.setAttribute('style', 'color: green')
+      labelUsuario.innerHTML = 'Usuário'
+      usuario.setAttribute('style', 'border-color: green')
+      validUsuario = true
+    }
+  })
+
+senha.addEventListener('keyup',()=> {
+    if(senha.value.length <= 7) {
+        labelSenha.setAttribute('style', 'color: red')
+        labelSenha.innerHTML = 'Senha *Insira no minimo 8 caracteres'
+        senha.setAttribute('style', 'border-color: red')
+        validSenha = false
+    } else if (!/[A-Z]/.test(senha.value)) {
+        labelSenha.setAttribute('style', 'color: red')
+        labelSenha.innerHTML = 'Senha *Insira pelo menos 1 caracteres maiúsculo'
+        senha.setAttribute('style', 'border-color: red')
+        validSenha = false
+    } else if (!/[\W_]/.test(senha.value)){
+        labelSenha.setAttribute('style', 'color: red')
+        labelSenha.innerHTML = 'Senha *Insira pelo menos 1 símbolo'
+        senha.setAttribute('style', 'border-color: red')
+        validSenha = false
+    } else {
+        labelSenha.setAttribute('style', 'color: green')
+        labelSenha.innerHTML = 'Senha'
+        senha.setAttribute('style', 'border-color: green')
+        validSenha = true
+    }
+  })
+
+confirmSenha.addEventListener('keyup', () => {
+    if(senha.value == confirmSenha.value){
+    labelConfirmSenha.setAttribute('style', 'color: green')
+      labelConfirmSenha.innerHTML = 'Confirmar Senha'
+      confirmSenha.setAttribute('style', 'border-color: green')
+      validConfirmSenha = true
+    } else {
+        labelConfirmSenha.setAttribute('style', 'color: red')
+        labelConfirmSenha.innerHTML = 'Confirmar Senha *As senhas não conferem'
+        confirmSenha.setAttribute('style', 'border-color: red')
+        validConfirmSenha = false
+    }
+  })
+  
+function cadastrar() {
+    if(validNome && validUsuario && validSenha && validConfirmSenha) {
+        let listaUser = JSON.parse(localStorage.getItem['listaUser'] || '[]')
+
+        let senhaCriptografada = Crypto.SHA512(senha.value).toString()
+
+        listaUser.push(
+            {
+                nomeCad: nome.value,
+                userCad: usuario.value,
+                senhaCad: senhaCriptografada
+            }
+        )
+        
+    }
+  
+    localStorage.setItem('listUser', JSON.stringify(listaUser))
 }
-)
